4b189f044afa580ebc2489ac8d4c72972f14b890
isaacscardoso <isaacscardosoblues@gmail.com>
2023-06-22 15:48:57 -0300

    Initial database implementation with SQFlite.

---
+++ blueshrine_classroom_app/lib/src/core/database/database_helper.dart
@@ -0,0 +1,42 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  static Database? _database;
+
+  DatabaseHelper._();
+
+  static DatabaseHelper get instance {
+    _instance ??= DatabaseHelper._();
+    return _instance!;
+  }
+
+  Future<Database> get database async {
+    return _database ?? await _initDatabase();
+  }
+
+  Future<Database> _initDatabase() async {
+    final database = await openDatabase(
+      join(await getDatabasesPath(), 'bsclassroom.db'),
+      version: 1,
+      onCreate: _onCreate,
+    );
+    return database;
+  }
+
+  Future<void> _onCreate(Database database, int version) async {
+    await database.execute(_studentsTable);
+  }
+
+  String get _studentsTable => '''
+    CREATE TABLE IF NOT EXISTS `students` (
+      `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+      `name` VARCHAR(60) NOT NULL,
+      `description` TEXT,
+      `status` ENUM('Active', 'Inactive') NOT NULL,
+      `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+  ''';
+}
