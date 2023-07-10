35bf07b7ab0d1cae94f1394054776018f49c123e
isaacscardoso <isaacscardosoblues@gmail.com>
2023-07-10 02:11:48 -0300

    (amend last commit)

--- blueshrine_classroom_app/lib/src/modules/student/student_module.dart
+++ blueshrine_classroom_app/lib/src/modules/student/student_module.dart
@@ -4,6 +4,7 @@ import './ui/pages/student_list_page.dart';
 import './ui/pages/student_form_page.dart';
 import './controllers/student_controller.dart';
 import './controllers/student_form_controller.dart';
+import './bloc/student_bloc.dart';
 
 class StudentModule extends Module {
   @override
@@ -11,6 +12,7 @@ class StudentModule extends Module {
     return <Bind<Object>>[
       Bind.lazySingleton((i) => StudentController(i())),
       Bind.lazySingleton((i) => StudentFormController(i())),
+      Bind.lazySingleton((i) => StudentBloc(i())),
     ];
   }
 
